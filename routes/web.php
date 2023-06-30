686dc3c9293e95b2336de562d836f27decd4dba5
BrianMiltonJr <harlanbmiltonjr@gmail.com>
2023-06-30 09:18:40 -0500

    Installed Front End
    - Svelte
    - TailwindCSS
    - i18n
    
    Also installed InertiaJS

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from 'path';
+import autoPreprocess from 'svelte-preprocess';
+const projectRootDir = resolve(__dirname);
 
 export default defineConfig({
     plugins: [
@@ -7,7 +11,23 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        svelte({
+            preprocess: autoPreprocess(),
+        })
     ],
+    optimizeDeps: {
+        include: [
+            '@inertiajs/inertia',
+            '@inertiajs/inertia-svelte',
+        ]
+    },
+    resolve: {
+        alias: {
+            '@': resolve(projectRootDir, 'resources/js'),
+            '~': resolve(projectRootDir, 'resources'),
+        },
+        extensions: ['.js', '.svelte', '.json'],
+    },
     server: {
         hmr: {
          host: 'localhost'
