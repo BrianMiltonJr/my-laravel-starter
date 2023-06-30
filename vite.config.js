554e317f7cfeb07e0798ede26f3c4825036dab87
BrianMiltonJr <harlanbmiltonjr@gmail.com>
2023-06-30 11:22:41 -0500

    removed duplicate css

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const projectRootDir = resolve(__dirname);
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
         svelte({
